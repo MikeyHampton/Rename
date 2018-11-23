164bf0c43e1e453f3c8f61d98f0c2b3aba1ae989
MikeyHampton <45147617+MikeyHampton@users.noreply.github.com>
2018-11-23 17:15:29 -0500

    now

--- src/Main/Main.php
+++ src/Main/Main.php
@@ -3,7 +3,7 @@ namespace Main;
 use pocketmine\Server;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
-class Main extends PluginBase implements Listener
+class Main extends PluginBase implements Listener{
     public function onEnable(): void
         $this->getServer()->getPluginManager()->registerEvents(($this), $this);
         $this->getLogger()->info("This Plugin Is Custom And Not Public You Can Buy This Plugin By Contacting Me On Discord: MikeyHampton#5530");
